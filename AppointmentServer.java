2404996130219dc46b944ab82660cd550d71ea20
Josua-Louw <u24569772@tuks.co.za>
2026-03-01 21:54:59 +0200

    modified data validation output and added colors

--- AppointmentServer.java
+++ AppointmentServer.java
@@ -14,6 +14,11 @@ public class AppointmentServer {
 
     private static final Pattern TIME_PATTERN =
         Pattern.compile("^([01]\\d|2[0-3]):[0-5]\\d$");
+    //Colors for terminal output
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
 
     public static void main(String[] args) throws IOException {
 
@@ -21,11 +26,12 @@ public class AppointmentServer {
 
         ServerSocket server = new ServerSocket(PORT);
         System.out.println("Server running on port " + PORT);
-
-        while (true) {
+        String input = "";
+        while (!input.equals("exit")) {
             Socket client = server.accept();
             handleClient(client);
         }
+        server.close();
     }
 
     private static void handleClient(Socket socket) {
@@ -37,32 +43,32 @@ public class AppointmentServer {
                     socket.getOutputStream(), true);
 
             clearScreen(out);
-            numLines += 1;
-            out.println("=== Appointment Manager ===");
 
             boolean running = true;
 
             while (running) {
                 numLines += 2;
                 moveCursor(out, numLines, 0);
-                out.write("\n1. Add Appointment");
+                out.write(ANSI_BLUE + "\n1. Add Appointment");
                 moveCursor(out, ++numLines, 0);
-                out.write("\n2. View Appointments");
+                out.write(ANSI_BLUE + "\n2. View Appointments");
                 moveCursor(out, ++numLines, 0);
-                out.write("\n3. Delete Appointment");
+                out.write(ANSI_BLUE + "\n3. Delete Appointment");
                 moveCursor(out, ++numLines, 0);
-                out.write("\n4. Search Appointments");
+                out.write(ANSI_BLUE + "\n4. Search Appointments");
                 moveCursor(out, ++numLines, 0);
-                out.write("\n5. Exit");
+                out.write(ANSI_BLUE + "\n5. Exit");
                 moveCursor(out, ++numLines, 0);
-                out.write("\nChoice: ");
+                out.write(ANSI_RESET + "\nChoice: ");
                 out.flush();
 
                 String choice = readAndEcho(in, out);
 
                 if (!choice.matches("[1-5]")) {
                     clearScreen(out);
-                    out.println("Invalid option. Please enter 1-5.");
+                    numLines += 1;
+                    moveCursor(out, numLines, 0);
+                    out.write(ANSI_RED + "Invalid option. Please enter 1-5.");
                     continue;
                 }
 
@@ -83,7 +89,7 @@ public class AppointmentServer {
                     running = false;
                 }
                 else {
-                    out.println("Invalid option.");
+                    out.println(ANSI_RED + "Invalid option.");
                 }
             }
 
@@ -115,56 +121,94 @@ public class AppointmentServer {
         String person;
         String desc;
 
+        boolean valid = true;
         // DATE VALIDATION
         while (true) {
             clearScreen(out);
-            out.print("Date (YYYY-MM-DD): ");
+            if (!valid) {
+                numLines += 1;
+                moveCursor(out, numLines, 0);
+                out.write(ANSI_RED + "Please enter a valid date. (enter quit to cancel)");
+            }
+            numLines += 1;
+            moveCursor(out, numLines, 0);
+            out.write(ANSI_RESET + "Date (YYYY-MM-DD): ");
             out.flush();
             date = readAndEcho(in, out);
 
-            if (DATE_PATTERN.matcher(date).matches())
+            if (DATE_PATTERN.matcher(date).matches() && isValidDate(date))
                 break;
-
-            out.println("Invalid date format. Try again.");
+            if (checkToCancel(date, out)) {
+                return;
+            }
+            valid = false;
         }
-
+        valid = true;
         // TIME VALIDATION
         while (true) {
             clearScreen(out);
-            out.print("Time (HH:MM): ");
+            if (!valid) {
+                numLines += 1;
+                moveCursor(out, numLines, 0);
+                out.write(ANSI_RED + "Please enter a valid time. (enter quit to cancel)");
+            }
+            numLines += 1;
+            moveCursor(out, numLines, 0);
+            out.write(ANSI_RESET + "Time (HH:MM): ");
             out.flush();
             time = readAndEcho(in, out);
 
-            if (TIME_PATTERN.matcher(time).matches())
+            if (TIME_PATTERN.matcher(time).matches() && isValidTime(time, date))
                 break;
 
-            out.println("Invalid time format. Use 24-hour format.");
+            if (checkToCancel(time, out)) {
+                return;
+            }
+            valid = false;
         }
-
+        valid = true;
         // PERSON VALIDATION
         while (true) {
             clearScreen(out);
-            out.print("With whom: ");
+            if (!valid) {
+                numLines += 1;
+                moveCursor(out, numLines, 0);
+                out.write(ANSI_RED + "Name cannot be empty. (enter quit to cancel)");
+            }
+            numLines += 1;
+            moveCursor(out, numLines, 0);
+            out.write(ANSI_RESET + "With whom: ");
             out.flush();
             person = readAndEcho(in, out);
 
             if (!person.trim().isEmpty())
                 break;
-
-            out.println("Name cannot be empty.");
+            if (checkToCancel(person, out)) {
+                return;
+            }
+            valid = false;
         }
-
+        valid = true;
         // DESCRIPTION VALIDATION
         while (true) {
             clearScreen(out);
-            out.print("Description: ");
+            if (!valid) {
+                numLines += 1;
+                moveCursor(out, numLines, 0);
+                out.write(ANSI_RED + "Description cannot be empty. (enter quit to cancel)");
+            }
+            numLines += 1;
+            moveCursor(out, numLines, 0);
+            out.write(ANSI_RESET + "Description: ");
             out.flush();
             desc = readAndEcho(in, out);
 
             if (!desc.trim().isEmpty())
                 break;
-
-            out.println("Description cannot be empty.");
+            if (checkToCancel(desc, out)) {
+                return;
+            }
+            valid = false;
         }
 
         String entry = date + "|" + time + "|" + person + "|" + desc;
@@ -172,23 +216,26 @@ public class AppointmentServer {
         saveToFile();
 
         clearScreen(out);
-        out.println("Appointment added successfully.");
+        numLines += 1;
+        moveCursor(out, numLines, 0);
+        out.write(ANSI_GREEN + "Appointment added successfully.");
     }
 
     private static void viewAppointments(PrintWriter out) {
         numLines += 1;
-        out.write("\n--- Appointments ---");
-
+        moveCursor(out, numLines, 33);
+        out.write(ANSI_BLUE + "\n--- Appointments ---");
+        numLines += 2;
         if (appointments.isEmpty()) {
             numLines += 1;
             moveCursor(out, numLines, 0);
-            out.write("No appointments found.");
+            out.write(ANSI_RED + "No appointments found.");
         }
         else {
             for (int i = 0; i < appointments.size(); i++) {
                 numLines += 1;
-                moveCursor(out, numLines, 0);
-                out.write((i + 1) + ". " + appointments.get(i));
+                moveCursor(out, numLines, 20);
+                out.write(ANSI_RESET + (i + 1) + ". " + appointments.get(i));
             }
         }
         out.flush();
@@ -203,13 +250,13 @@ public class AppointmentServer {
             return;
         numLines += 2;
         moveCursor(out, numLines, 0);
-        out.write("Enter number to delete: ");
+        out.write(ANSI_RESET + "Enter number to delete: ");
         out.flush();
         String input = readAndEcho(in, out);
 
         try {
             if (!input.matches("\\d+")) {
-                out.write("Please enter a valid number.");
+                out.write(ANSI_RED + "Please enter a valid number.");
                 out.flush();
                 return;
             }
@@ -219,14 +266,14 @@ public class AppointmentServer {
             if (index >= 0 && index < appointments.size()) {
                 appointments.remove(index);
                 saveToFile();
-                out.write("Deleted.");
+                out.write(ANSI_GREEN + "Deleted.");
             }
             else {
-                out.write("Invalid number.");
+                out.write(ANSI_RED + "Invalid number.");
             }
         }
         catch (NumberFormatException e) {
-            out.write("Invalid input.");
+            out.write(ANSI_RED + "Invalid input.");
         }
         out.flush();
     }
@@ -235,21 +282,23 @@ public class AppointmentServer {
             throws IOException {
 
         clearScreen(out);
-        out.print("Enter search term: ");
+        numLines += 1;
+        moveCursor(out, numLines, 0);
+        out.print(ANSI_RESET + "Enter search term: ");
         out.flush();
         String term = readAndEcho(in, out);
 
+        numLines += 2;
+        moveCursor(out, numLines, 32);
+        out.write(ANSI_BLUE + "--- Search Results ---");
         numLines += 1;
-        moveCursor(out, numLines, 0);
-        out.write("\n--- Search Results ---");
-
         int count = 0;
         Pattern pattern = Pattern.compile(Pattern.quote(term), Pattern.CASE_INSENSITIVE);
         for (String entry : appointments) {
             if (pattern.matcher(entry).find()) {
                 numLines += 1;
-                moveCursor(out, numLines, 0);
-                out.write(entry);
+                moveCursor(out, numLines, 20);
+                out.write(ANSI_RESET + entry);
                 count++;
             }
         }
@@ -257,7 +306,7 @@ public class AppointmentServer {
         if (count == 0) {
             numLines += 1;
             moveCursor(out, numLines, 0);
-            out.write("No matching appointments found.");
+            out.write(ANSI_RED + "No matching appointments found.");
         }
         out.flush();
     }
@@ -306,11 +355,46 @@ public class AppointmentServer {
         out.print("\033[2J");
         out.print("\033[H");
         out.flush();
+        numLines += 1;
+        moveCursor(out, 0, 30);
+        out.write(ANSI_RESET + "=== Appointment Manager ===");
     }
 
     private static void moveCursor(PrintWriter out, int row, int col) {
         out.write(27);
         out.write("[" + row + ";" + col + "H");
         out.flush();
+    }
+
+    //Only schedule appointments for future dates
+    private static boolean isValidDate(String date) {
+        String[] parts = date.split("-");
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+        return new Date().compareTo(new Date(year - 1900, month - 1, day)) < 0;
+    }
+
+    //Search through file to check if time is already taken for that date
+    private static boolean isValidTime(String time, String date) {
+        for (String entry : appointments) {
+            String[] parts = entry.split("\\|");
+            if (parts[0].equals(date) && parts[1].equals(time)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkToCancel(String input, PrintWriter out) {
+        if (input.equals("quit"))
+        {
+            clearScreen(out);
+            numLines += 1;
+            moveCursor(out, numLines, 0);
+            out.write(ANSI_RED + "Appointment cancelled.");
+            return true;
+        }
+        return false;
     }
 }
